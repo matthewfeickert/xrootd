7d796473f7b9e4ea9bf9fb4665ca370b24220ba0
Matthew Feickert <matthew.feickert@cern.ch>
2022-01-13 16:01:44 -0600

    Add check to ensure wheel is avaialble so no errors

--- packaging/wheel/publish.sh
+++ packaging/wheel/publish.sh
@@ -5,4 +5,20 @@ version=$(./genversion.sh --print-only)
 version=${version#v}
 echo $version > bindings/python/VERSION
 rm -r dist
+
+# Determine if wheel.bdist_wheel is available for wheel.bdist_wheel in setup.py
+python3 -c 'import wheel' &> /dev/null
+wheel_available=$?
+if [ "${wheel_available}" -ne "0" ]; then
+    python3 -m pip install wheel
+    res=$?
+fi
+unset wheel_available
+
+if [ "$res" -ne "0" ]; then
+    echo "ERROR: Unable to find wheel and unable to install wheel with '$(command -v python3) -m pip install wheel'."
+    echo "       Please ensure wheel is available to $(command -v python3) and try again."
+    exit 1
+fi
+
 python3 setup.py sdist
