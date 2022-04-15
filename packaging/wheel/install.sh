222e43ec75f83dd7be3cd20229faaa98a8b0b9f0
Matthew Feickert <matthew.feickert@cern.ch>
2022-04-15 16:17:06 -0500

    Use python -m wheel to convert to egg to wheel

--- packaging/wheel/install.sh
+++ packaging/wheel/install.sh
@@ -58,27 +58,44 @@ fi
 cd $startdir
 rm -r xrootdbuild
 
+
+printf "\n\n\n\n\n#DEBUG\n\n\n\n\n"
+ls -lhtra
+printf "\n\n\n\n\n#DEBUG\n\n\n\n\n"
+echo "INPUT 1: ${1}"
+printf "\n\n\n\n\n#DEBUG\n\n\n\n\n"
+
 # TODO: Remove all of the below and build a wheel using PyPA tools
 
-# convert the egg-info into a proper dist-info
-egginfo_path=$(ls $1/xrootd-*.egg-info)
-core="${egginfo_path%.*}"
-core="${egginfo_path#$1/}"
-sufix="${core#xrootd-*.*.*_-}"
-core="${core%_-*}"
-if [[ "$core" == "$sufix" ]]
-then
-    distinfo_path="${egginfo_path%.*}.dist-info"
-else
-    distinfo_path="$1/$core-$sufix"
-fi
-echo $distinfo_path >> /tmp/out.txt
-mkdir $distinfo_path
-mv $egginfo_path $distinfo_path/METADATA
-echo -e "Wheel-Version: 1.0\nGenerator: bdist_wheel (0.35.1)\nRoot-Is-Purelib: true\nTag: py2-none-any\nTag: py3-none-any" > $distinfo_path/WHEEL
-touch $distinfo_path/RECORD
-distinfo_name=${distinfo_path#"$1"}
-find $1/pyxrootd/      -type f -exec sha256sum {} \; | awk '{printf$2 ",sha256=" $1 "," ; system("stat --printf=\"%s\" "$2) ; print '\n'; }' >> $distinfo_path/RECORD
-find $1/$distinfo_name -type f -exec sha256sum {} \; | awk '{printf$2 ",sha256=" $1 "," ; system("stat --printf=\"%s\" "$2) ; print '\n'; }' >> $distinfo_path/RECORD
-find $1/XRootD/        -type f -exec sha256sum {} \; | awk '{printf$2 ",sha256=" $1 "," ; system("stat --printf=\"%s\" "$2) ; print '\n'; }' >> $distinfo_path/RECORD
-find $1/pyxrootd/ -type l | awk '{print$1 ",,"}' >> $distinfo_path/RECORD
+python -m wheel convert \
+    --verbose \
+    --dest-dir /tmp \
+    $(python -m pip show xrootd | grep Location | awk '{print $NF}')
+# Need to replace cp3x with 'none' in the wheel name (e.g. xrootd-2022.415-py310-none-linux_x86_64.whl)
+mv /tmp/xrootd-*.whl "$(ls /tmp/xrootd-*.whl | sed 's/cp[0-9]*-/none-/g')"
+python -m pip uninstall --yes --verbose xrootd
+# After uninstall there will now be an empty easy-install.path left over from egg build
+python -m pip install --upgrade /tmp/xrootd-*.whl
+
+# # convert the egg-info into a proper dist-info
+# egginfo_path=$(ls $1/xrootd-*.egg-info)
+# core="${egginfo_path%.*}"
+# core="${egginfo_path#$1/}"
+# sufix="${core#xrootd-*.*.*_-}"
+# core="${core%_-*}"
+# if [[ "$core" == "$sufix" ]]
+# then
+#     distinfo_path="${egginfo_path%.*}.dist-info"
+# else
+#     distinfo_path="$1/$core-$sufix"
+# fi
+# echo $distinfo_path >> /tmp/out.txt
+# mkdir $distinfo_path
+# mv $egginfo_path $distinfo_path/METADATA
+# echo -e "Wheel-Version: 1.0\nGenerator: bdist_wheel (0.35.1)\nRoot-Is-Purelib: true\nTag: py2-none-any\nTag: py3-none-any" > $distinfo_path/WHEEL
+# touch $distinfo_path/RECORD
+# distinfo_name=${distinfo_path#"$1"}
+# find $1/pyxrootd/      -type f -exec sha256sum {} \; | awk '{printf$2 ",sha256=" $1 "," ; system("stat --printf=\"%s\" "$2) ; print '\n'; }' >> $distinfo_path/RECORD
+# find $1/$distinfo_name -type f -exec sha256sum {} \; | awk '{printf$2 ",sha256=" $1 "," ; system("stat --printf=\"%s\" "$2) ; print '\n'; }' >> $distinfo_path/RECORD
+# find $1/XRootD/        -type f -exec sha256sum {} \; | awk '{printf$2 ",sha256=" $1 "," ; system("stat --printf=\"%s\" "$2) ; print '\n'; }' >> $distinfo_path/RECORD
+# find $1/pyxrootd/ -type l | awk '{print$1 ",,"}' >> $distinfo_path/RECORD
